72a0dc13e7662e0827bd4f0f0e32a8cc29327e88
aanaishe <ahegab@uthsc.edu>
2015-09-12 19:47:41 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,5 +1,4 @@
-library(dplyr)
-
+## Download the data
 url <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
 
 if (!file.exists('Coursera/exdata-data-household_power_consumption.zip')) {
@@ -7,20 +6,25 @@ if (!file.exists('Coursera/exdata-data-household_power_consumption.zip')) {
   unz('Coursera/exdata-data-household_power_consumption.zip', exdir='./Coursera')
 }
 
+## Read the data into R
 HPCData <- read.csv2("Coursera/household_power_consumption.txt", na.strings="?", stringsAsFactors = FALSE)
 head(HPCData)
 
+## Select the information that corresponds to the desired dates "2007-02-01" and "2007-02-02"
 HPCData[,"Date"] <- as.Date(HPCData[,"Date"], "%d/%m/%Y")
 DesiredDates = as.Date(c("2007-02-01","2007-02-02"))
 HPCData2 = subset(HPCData, HPCData[,"Date"] %in% DesiredDates)
 
+## Change the class of "Global_active_power", "Voltage", and "Global_reactive_power" from character to numeric
 HPCData2[,"Global_active_power"] <- as.numeric(HPCData2[,"Global_active_power"])
 HPCData2[,"Voltage"] <- as.numeric(HPCData2[,"Voltage"])
 HPCData2[,"Global_reactive_power"] <- as.numeric(HPCData2[,"Global_reactive_power"])
 
+## Change the class of date and time to "POSIXct
 DT <- paste(as.Date(HPCData2$Date), HPCData2$Time)
 HPCData2$DT <- as.POSIXct(DT)
 
+## Generate plot4 and save as a .png file
 png("plot4.png",width=480,height=480,units="px")
 
 par(mfrow = c(2, 2))
